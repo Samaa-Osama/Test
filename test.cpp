3b8e1af883a1ce75c2ed884771c4c34b2354db60
samaaosama <samaaosama107@gmail.com>
2024-07-22 22:46:20 +0300

    test

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define InRange(num,min,max)(num >= min && num <= max)
+#define HalfRange(num,min,max)(num > min && num < max)
+#define min(x,y) ((x) < (y)) ? (x) : (y)
+#define max(x,y) ((x) > (y)) ? (x) : (y) 
+
+
+
+int main(){
+    fast;
+    freopen("file.in","r",stdin);
+    freopen("output.txt","w",stdout);
+    
+    return 0;
+}
